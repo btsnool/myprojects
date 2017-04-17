39907a542f1866ed23545afd977afeee17de86e0
wj <wj@DESKTOP-FJCJI00>
2017-04-18 00:14:14 +0800

    new Internal ProxyServer

--- ReverseProxy/src/me/selfproject/internalserver/InternalProxyServer.java
+++ ReverseProxy/src/me/selfproject/internalserver/InternalProxyServer.java
@@ -1,6 +1,18 @@
 package me.selfproject.internalserver;
 
 public class InternalProxyServer {
+	
+	private String remoteServerIP;
+	private int remoteServerPort;
+	
+	public InternalProxyServer(String remoteServerIP , int remoteServerPort){
+		
+		this.remoteServerIP = remoteServerIP;
+		this.remoteServerPort = remoteServerPort;
+		
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
