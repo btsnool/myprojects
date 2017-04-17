ab6821874c6ae8945b296a920275340f078a4b94
wj <wj@DESKTOP-FJCJI00>
2017-04-18 00:13:38 +0800

    add new proxy handle

---
+++ ReverseProxy/src/me/selfproject/internalserver/InternalProxyServerHandle.java
@@ -0,0 +1,5 @@
+package me.selfproject.internalserver;
+
+public class InternalProxyServerHandle {
+
+}
--- ReverseProxy/src/me/selfproject/server/ProxyServerHandle.java
+++ ReverseProxy/src/me/selfproject/server/ProxyServerHandle.java
@@ -1,9 +1,13 @@
 package me.selfproject.server;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import me.selfproject.constants.CommonConstants;
 
@@ -11,6 +15,7 @@ public class ProxyServerHandle implements Runnable {
 
 	private Socket socket;
 	
+	private LinkedBlockingQueue<Socket> socketPool = new LinkedBlockingQueue<Socket>();
 	
 	
 	public  ProxyServerHandle(Socket socket) {
@@ -44,12 +49,33 @@ public class ProxyServerHandle implements Runnable {
 			
 			String msg = new String(data,"UTF-8");
 			if(msg.equals(CommonConstants.MSG_NEWCONN)){
+				try {
+					socketPool.put(socket);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				OutputStream out = socket.getOutputStream();
+				out.write(new String(CommonConstants.MSG_OK).getBytes());
 				
 			}else{
 				
+				Socket socket_ = socketPool.poll();
+				OutputStream out = socket_.getOutputStream();
+				out.write(data);
+				out.flush();
 				
+				BufferedInputStream in = new BufferedInputStream(socket_.getInputStream());
+				
+				byte[] buffer_ = new byte[1024];
+				int readingSize = -1;
+				while((readingSize = in.read(buffer_))!=-1){
+					new BufferedOutputStream(socket.getOutputStream()).write(buffer_, 0, readingSize);
+				}
+				socket.close();
+				socketPool.put(socket_);
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			try {
